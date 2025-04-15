f0f4518f407022bf5c683b46bab7f773325452ea
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2025-04-15 21:45:34 +0800

    Update PC stats scripts.

--- scripts/pcstats.py
+++ scripts/pcstats.py
@@ -9,7 +9,5 @@ def get_pc_stats():
         "Disk Usage": f"{psutil.disk_usage('/').percent}%"
     }
 
-while True:
-    stats = get_pc_stats()
-    print(json.dumps(stats))  # Print the stats as JSON
-    time.sleep(60)  # wait for a minute before updating again
+stats = get_pc_stats()
+print(json.dumps(stats))  # Print the stats as JSON
--- scripts/sendstats.sh
+++ scripts/sendstats.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+source ~/Desktop/CAVE/pipeline-project-AnuKritiW/venv/bin/activate
 while true; do
-    python3 ~/pipeline-project-AnuKritiW/scripts/pcstats.py | ssh pi@192.168.181.44 "cat > ~/pipeline-project-AnuKritiW/out/pcstats.json"
-    # | (pipe) passes the JSON stats from pcstats.py to the next command
-    sleep 120  # Update every two minutes
+    echo "[INFO] Running pcstats.py..."
+    STATS=$(python3 ~/Desktop/CAVE/pipeline-project-AnuKritiW/scripts/pcstats.py)
+    echo "TEST"
+
+    if [[ -z "$STATS" ]]; then
+        echo "[ERROR] No stats received!"
+    else
+        echo "Captured stats: $STATS"
+        echo "$STATS" | ssh pi@pi.local "cat > ~/pipeline-project-AnuKritiW/out/pcstats.json"
+    fi
+
+    sleep 10
 done
