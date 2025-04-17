3f11ae61eda5f73c9319628ca54de9249d102a41
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2025-04-17 13:58:29 +0800

    Make basic profiles webpage + trigger stats script.

--- web-app/app.py
+++ web-app/app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import subprocess
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
-@app.route('/')
+PROFILES = {
+    "stats": {
+        "name": "System Stats",
+        "icon": "📊",
+        "script": "stats-test.py"
+    },
+    "image1": {
+        "name": "Image Display",
+        "icon": "🖼️",
+        "script": "display-image.py"
+    }
+}
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return 'Hello world'
+    if request.method == "POST":
+        profile = request.form["profile"]
+        subprocess.Popen(["/home/pi/.virtualenvs/pimoroni/bin/python3", f"/home/pi/pipeline-project-AnuKritiW/scripts/{PROFILES[profile]['script']}"])
+        with open("selected_profile.txt", "w") as f:
+            f.write(profile)
+        return redirect("/")
+    return render_template("index.html", profiles=PROFILES)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
