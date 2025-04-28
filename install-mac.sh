5126862c1357fcded7097e9a1162d4f8aa9d2316
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2025-04-28 01:01:05 +0100

    Add install scripts.

---
+++ install-mac.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Installing required Python packages for pcstats..."
+pip3 install psutil
+
+echo "✅ Mac stats sender is ready."
+# echo "Run it like: python3 pcstats.py --send-to YOUR_PI_IP"
